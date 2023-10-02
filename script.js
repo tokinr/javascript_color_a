70b56f33687008e3cec1e410fc945bec713d448c
tokinr <82437697+tokinr@users.noreply.github.com>
2023-10-02 15:17:37 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// JavaScript
+
+const color = document.getElementById('myColor');
+const text = document.getElementById('bodyText');
+
+//背景色を変更する関数
+//function構文を使う書き方
+function changeColor(){
+  document.body.style.backgroundColor = color.value;
+
+  //カラーコードを表示
+  if (color.value === '#ffffff') {
+    text.textContent = 'コード：' + color.value + '(white)';
+  } else if (color.value === '#000000') {
+    text.textContent = 'コード：' + color.value + '(black)';
+  } else {
+    text.textContent = 'コード：' + color.value;
+  }
+
+}
+
+//アロー関数を使う書き方
+// const changeColor = () => {
+//   document.body.style.backgroundColor = color.value;
+//   text.textContent = color.value;
+// }
+
+// カラーピッカーが変更されたときに、背景色を変える
+color.addEventListener('input', changeColor);
